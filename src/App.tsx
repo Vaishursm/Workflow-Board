2fccbaf7685d77f4b1498dc3dbc6d79109315e68
Vaishali Sornamani <vsornamani@altimetrik.com>
2026-03-13 20:10:27 +0530

    setup tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind Working
+      </h1>
+    </div>
+  );
+}
+
+export default App
